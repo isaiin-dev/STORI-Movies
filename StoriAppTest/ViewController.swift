3d414531d629295bf0b8bb56b00556ed3a0aa219
Alejandro Acosta <isaiin.dev@gmail.com>
2024-11-04 15:25:57 -0600

    Initial Commit

---
+++ StoriAppTest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  StoriAppTest
+//
+//  Created by Alejandro Isai Acosta Martinez on 04/11/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ StoriAppTestTests/StoriAppTestTests.swift
@@ -0,0 +1,17 @@
+//
+//  StoriAppTestTests.swift
+//  StoriAppTestTests
+//
+//  Created by Alejandro Isai Acosta Martinez on 04/11/24.
+//
+
+import Testing
+@testable import StoriAppTest
+
+struct StoriAppTestTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
